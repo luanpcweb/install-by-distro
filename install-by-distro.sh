00f2a0babc1ccb7b42bfb17199f05188960e1cef
Luan P. Coelho <luan@byteslivres.com.br>
2020-07-15 10:05:34 -0300

    Adjust versions and test

--- install-by-distro.sh
+++ install-by-distro.sh
@@ -1,16 +1,22 @@
-DISTRO=$( cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(debian|ubuntu|red hat|centos|nameyourdistro)' | uniq )
+#!/bin/bash
+
+DISTRO=$( cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(debian|ubuntu|red hat|centos)' | uniq )
 if [ -z $DISTRO ]; then
     DISTRO='unknown'
     echo "Unknown"
 fi
 echo "Detected Linux distribution: $DISTRO"
 
-if [ "$DISTRO" = "debian" ]; then
+if [[ "$DISTRO" = "debian" ]]; then
     echo "Install Debian"
     apt-get update -y
 
-elif [ "$DISTRO" = "centos" ]; then
+elif [[ "$DISTRO" = "centos" ]]; then
     echo "Install Centos"
+    yum update -y
+
+elif [[ "$DISTRO" = "ubuntu" ]]; then
+    echo "Install Ubuntu"
     apt-get update -y
 
 else
